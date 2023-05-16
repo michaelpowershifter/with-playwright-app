d07c6f0d47a2d52d04631ca31955c479b233d42b
401asfg <mb.allan@ymail.com>
2023-05-15 17:10:34 -0700

    Added new tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -23,6 +23,45 @@ test('should show a hidden about page element on mouse over', async ({ page }) =
   await expect(hiddenDiv).toBeVisible()
 })
 
+test('should input text into a text box and read that text', async ({ page }) => {
+  await page.goto('/about')
+
+  const inputBox = page.locator('input')
+
+  await expect(inputBox).toHaveValue("")
+
+  await inputBox.type("This is a test")
+  await expect(inputBox).toHaveValue("This is a test")
+
+  await inputBox.type("This is another test")
+  await inputBox.press("Enter")
+  await expect(inputBox).toHaveValue("This is a testThis is another test")
+})
+
+test('should only show drop down contents after the drop down button is clicked', async ({ page }) => {
+  await page.goto('/about')
+
+  const actionOne = page.getByRole('link', { name: 'Action', exact: true })
+  const actionTwo = page.getByRole('link', { name: 'Another action', exact: true })
+  const actionThree = page.getByRole('link', { name: 'Something else', exact: true })
+
+  await expect(actionOne).not.toBeVisible()
+  await expect(actionTwo).not.toBeVisible()
+  await expect(actionThree).not.toBeVisible()
+
+  const dropdownButton = page.locator('button')
+
+  await dropdownButton.click()
+
+  await expect(actionOne).toBeVisible()
+  await expect(actionTwo).toBeVisible()
+  await expect(actionThree).toBeVisible()
+
+  await actionOne.click()
+  await actionTwo.click()
+  await actionThree.click()
+})
+
 test('should navigate back to the home page', async ({ page }) => {
   await page.goto('/about')
   await page.click('text=Go Back')
--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,22 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 export default function About() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>About Page</h1>
-        <h1>About Page</h1>
         <p className={styles.description}>
           <Link href="/">&larr; Go Back</Link>
         </p>
+        <input type="text" name="name" />
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
         <div className={styles.myDIV}>Hover over me</div>
         <div className={styles.hide}>I was hidden</div>
       </main>
